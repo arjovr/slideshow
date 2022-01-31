eadeb55594838f7f06907a21528f394d8668ac1c
Sebastian J. Ferrara <sebastian.ferrara@r2software.net>
2022-01-31 20:58:04 -0300

    initial commit

---
+++ index.js
@@ -0,0 +1,25 @@
+const imgs = document.querySelectorAll('.container > .img');
+
+function next() {
+    const current = document.querySelector('.container > .img.current');
+    let next = current.nextElementSibling;
+    if (!next) {
+        next = imgs[0];
+    }
+    if (next) {
+        current.classList.remove('current');
+        next.classList.add('current');
+    }
+}
+
+function prev() {
+    const current = document.querySelector('.container > .img.current');
+    let prev = current.previousElementSibling;
+    if (!prev) {
+        prev = imgs[imgs.length - 1];
+    }
+    if (prev) {
+        current.classList.remove('current');
+        prev.classList.add('current');
+    }
+}
